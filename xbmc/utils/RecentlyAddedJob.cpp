79da0afe1bc2bbb7e5fe4cd97842410f02ada535
Memphiz <memphis@machzwo.de>
2016-02-06 14:34:40 +0100

    [RecentlyAddedJob/tvos] - on tvos inform the topshelf object about recently added stuff...

--- xbmc/utils/RecentlyAddedJob.cpp
+++ xbmc/utils/RecentlyAddedJob.cpp
@@ -34,6 +34,11 @@
 #include "video/VideoThumbLoader.h"
 #include "xbmc/settings/Settings.h"
 
+#if defined(TARGET_DARWIN_TVOS)
+#include "platform/darwin/DarwinUtils.h"
+#include "platform/darwin/tvos/TVOSTopShelf.h"
+#endif
+
 #define NUM_ITEMS 10
 
 CRecentlyAddedJob::CRecentlyAddedJob(int flag)
@@ -146,7 +151,12 @@ bool CRecentlyAddedJob::UpdateVideo()
     home->SetProperty("LatestEpisode." + value + ".ShowThumb"     , "");
     home->SetProperty("LatestEpisode." + value + ".SeasonThumb"   , "");
     home->SetProperty("LatestEpisode." + value + ".Fanart"        , "");
-  }  
+  }
+  
+  #if defined(TARGET_DARWIN_TVOS)
+    // send recently added Movies and TvShows to TopShelf
+    CTVOSTopShelf::GetInstance().SetTopShelfItems(items, TVShowItems);
+  #endif
 
   i = 0;
   CFileItemList MusicVideoItems;
