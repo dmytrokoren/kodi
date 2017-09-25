dc37470afce18d1d8ad8551429ee047df850cfa2
Memphiz <memphis@machzwo.de>
2017-09-25 21:12:59 +0200

    [libexif] - fixed compile with tvos 11 (use std::min and std::max)

--- lib/libexif/ExifParse.cpp
+++ lib/libexif/ExifParse.cpp
@@ -34,8 +34,7 @@
 #else
 #include <memory.h>
 #include <cstring>
-#define min(a,b) (a)>(b)?(b):(a)
-#define max(a,b) (a)<(b)?(b):(a)
+#include <algorithm>
 #endif
 #include <math.h>
 #include <stdio.h>
@@ -413,8 +412,8 @@ void CExifParse::ProcessDir(const unsigned char* const DirStart,
     {
       case TAG_DESCRIPTION:
       {
-        int length = max(ByteCount, 0);
-        length = min(length, MAX_COMMENT);
+        int length = std::max(ByteCount, 0);
+        length = std::min(length, MAX_COMMENT);
         strncpy(m_ExifInfo->Description, (char *)ValuePtr, length);
         m_ExifInfo->Description[length] = '\0';
         break;
@@ -501,7 +500,7 @@ void CExifParse::ProcessDir(const unsigned char* const DirStart,
             m_ExifInfo->CommentsCharset = EXIF_COMMENT_CHARSET_JIS;
 
           int length = ByteCount - EXIF_COMMENT_CHARSET_LENGTH;
-          length = min(length, MAX_COMMENT);
+          length = std::min(length, MAX_COMMENT);
           memcpy(m_ExifInfo->Comments, ValuePtr + EXIF_COMMENT_CHARSET_LENGTH, length);
           m_ExifInfo->Comments[length] = '\0';
 //          FixComment(comment);                          // Ensure comment is printable
@@ -513,7 +512,7 @@ void CExifParse::ProcessDir(const unsigned char* const DirStart,
       {
         // The XP user comment field is always unicode (UCS-2) encoded
         m_ExifInfo->XPCommentsCharset = EXIF_COMMENT_CHARSET_UNICODE;
-        size_t length = min(ByteCount, MAX_COMMENT);
+        size_t length = std::min(ByteCount, MAX_COMMENT);
         memcpy(m_ExifInfo->XPComment, ValuePtr, length);
         m_ExifInfo->XPComment[length] = '\0';
       }
